33393f449b45693fe91587f1418ec17c423f51e7
Jan Deriu <jan.deriu@googlemail.com>
2020-02-04 14:34:48 +0100

    trans

--- run_transformer_rank_experiments.sh
+++ run_transformer_rank_experiments.sh
@@ -7,7 +7,7 @@ do
     do
       for opt in "adam" "sgd"
       do
-        python -m parlai.scripts.train_model -t dailydialog -m transformer/ranker -mf tmp/dailydialog/transformer/model_tr_"$nl"_"$nhead"_"$hid"_"$opt" -nl $nl -hid $hid -nhead $nhead -opt $opt -esz 300 --num-epochs 50 -veps 2.0 -bs 64 -lr 0.001 --dropout 0.1 --embedding-type fasttext_cc  -vp 10 --candidates batch --eval-candidates batch
+        python -m parlai.scripts.train_model -t dailydialog -m transformer/ranker -mf tmp/dailydialog/transformer/model_tr_"$nl"_"$nhead"_"$hid"_"$opt" -nl $nl -hid $hid -n-heads $nhead -opt $opt -esz 300 --num-epochs 50 -veps 2.0 -bs 64 -lr 0.001 --dropout 0.1 --embedding-type fasttext_cc  -vp 10 --candidates batch --eval-candidates batch
       done
     done
   done
