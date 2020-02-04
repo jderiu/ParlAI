9de9b0b1ed4be87d5f31b0a81a64f627c56eeb96
Jan Deriu <deri@zhaw.ch>
2020-02-04 16:18:34 +0100

    bleu

--- run_seq2seq_att_experiments.sh
+++ run_seq2seq_att_experiments.sh
@@ -5,7 +5,8 @@ do
   do
     for rnn in "gru" "lstm"
     do
-      python -m parlai.scripts.train_model -t dailydialog -m seq2seq -mf tmp/dailydialog/seq2seq_att/model_s2s_"$nl"_"$hs"_"$rnn"_bi -nl $nl -hs $hs -rnn $rnn -esz 300 -bi True -att general --num-epochs 50 -veps 2.0 -bs 64 -opt adam -lr 0.001 --dropout 0.1 --embedding-type fasttext_cc  -vp 10 --inference greedy -vmt ppl
+      python -m parlai.scripts.train_model -t dailydialog -m seq2seq -mf tmp/dailydialog/seq2seq_att/model_s2s_"$nl"_"$hs"_"$rnn"_bi -nl $nl -hs $hs -rnn $rnn -esz 300 -bi True -att general --num-epochs 50 -veps 2.0 -bs 64 -opt adam -lr 0.001 --dropout 0.1 --embedding-type fasttext_cc  -vp 10 --inference greedy -vmt bleu-4
+      python -m parlai.scripts.self_chat -t dailydialog:selfchat -m seq2seq -mf tmp/dailydialog/seq2seq_att/model_s2s_"$nl"_"$hs"_"$rnn"_bi
     done
   done
 done
