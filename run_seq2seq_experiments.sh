03c87523291aee558d3cfb9ed09090d2cc881f55
Jan Deriu <jan.deriu@googlemail.com>
2020-02-04 08:29:41 +0100

    test

--- run_seq2seq_experiments.sh
+++ run_seq2seq_experiments.sh
@@ -5,7 +5,7 @@ do
   do
     for rnn in "gru" "lstm"
     do
-      python -m parlai.scripts.train_model -t dailydialog -m seq2seq -mf /tmp/dailydialog/seq2seq/model_s2s_"$nl"_"$hs"_"$rnn"_bi -nl $nl -hs $hs -rnn $rnn -esz 300 -bi True -att none --num-epochs 1 -veps 2.0 -bs 64 -opt adam -lr 0.001 --dropout 0.1 --embedding-type fasttext_cc  -vp 10 --inference nucleus -vmt ppl
+      python -m parlai.scripts.train_model -t dailydialog -m seq2seq -mf tmp/dailydialog/seq2seq/model_s2s_"$nl"_"$hs"_"$rnn"_bi -nl $nl -hs $hs -rnn $rnn -esz 300 -bi True -att none --num-epochs 1 -veps 2.0 -bs 64 -opt adam -lr 0.001 --dropout 0.1 --embedding-type fasttext_cc  -vp 10 --inference nucleus -vmt ppl
     done
   done
 done
