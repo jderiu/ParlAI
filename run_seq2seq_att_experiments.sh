dffea835b2c56ede5df004d4f08b240c25758ed7
Jan Deriu <jan.deriu@googlemail.com>
2020-02-05 13:32:05 +0100

    larege experiment

--- run_seq2seq_att_experiments.sh
+++ run_seq2seq_att_experiments.sh
@@ -4,7 +4,7 @@ for nl in 2 4
 do
   for hs in 512 1024
   do
-    python -m parlai.scripts.train_model -t dailydialog -m seq2seq -mf tmp/dailydialog/seq2seq_att/model_s2s_"$nl"_"$hs"_gru_bi -nl $nl -hs $hs -rnn gru -esz 300 -bi true -att general --num-epochs 60 -veps 2.0 -bs 32 -opt adam --lr-scheduler invsqrt -lr 0.005  --dropout 0.1 --embedding-type fasttext_cc  -vp 10 --inference greedy -vmt bleu-4 --warmup-updates 4000 --invsqrt-lr-decay-gamma 1
+    python -m parlai.scripts.train_model -t dailydialog -m seq2seq -mf tmp/dailydialog/seq2seq_att/model_s2s_"$nl"_"$hs"_gru_bi -nl $nl -hs $hs -rnn gru -esz 300 -bi true -att general --num-epochs 60 -veps 2.0 -bs 32 -opt adam -lr 0.005  --dropout 0.1 --embedding-type fasttext_cc  -vp 10 --inference greedy -vmt bleu-4 --warmup-updates 4000
     python parlai/scripts/self_chat.py -t dailydialog:selfchat -m seq2seq -mf tmp/dailydialog/seq2seq_att/model_s2s_"$nl"_"$hs"_gru_bi --outfile tmp/dailydialog/model_s2s_att_"$nl"_"$hs"_gru_bi.json
   done
 done
