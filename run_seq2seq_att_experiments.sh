b5b8441983c3dcbba4a539d28e6228c6111befb5
Jan Deriu <jan.deriu@googlemail.com>
2020-02-05 10:26:01 +0100

    attention new experimetns

--- run_seq2seq_att_experiments.sh
+++ run_seq2seq_att_experiments.sh
@@ -1,12 +1,9 @@
 ##!/usr/bin/env bash
-for nl in 1 2
+for nl in 2 4
 do
-  for hs in 128 256 512
+  for hs in 512 1024
   do
-    for rnn in "gru" "lstm"
-    do
-      python -m parlai.scripts.train_model -t dailydialog -m seq2seq -mf tmp/dailydialog/seq2seq_att/model_s2s_"$nl"_"$hs"_"$rnn"_bi -nl $nl -hs $hs -rnn $rnn -esz 300 -bi True -att general --num-epochs 50 -veps 2.0 -bs 64 -opt adam -lr 0.001 --dropout 0.1 --embedding-type fasttext_cc  -vp 10 --inference greedy -vmt bleu-4
-      python -m parlai.scripts.self_chat -t dailydialog:selfchat -m seq2seq -mf tmp/dailydialog/seq2seq_att/model_s2s_"$nl"_"$hs"_"$rnn"_bi
-    done
+    python -m parlai.scripts.train_model -t dailydialog -m seq2seq -mf tmp/dailydialog/seq2seq_att/model_s2s_"$nl"_"$hs"_gru_bi -nl $nl -hs $hs -rnn gru -esz 300 -bi true -att general --num-epochs 1 -veps 2.0 -bs 32 -opt adam --lr-scheduler invsqrt -lr 0.005 --dropout 0.1 --embedding-type fasttext_cc  -vp 10 --inference greedy -vmt bleu-4 --warmup-updates 4000
+    python -m parlai.scripts.self_chat -t dailydialog:selfchat -m seq2seq -mf tmp/dailydialog/seq2seq_att/model_s2s_"$nl"_"$hs"_gru_bi
   done
 done
