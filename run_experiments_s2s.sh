a41e98c0be9228f6db3929aa75183f5e2e7ae551
Jan Deriu <deri@zhaw.ch>
2020-02-07 13:44:18 +0100

    test

---
+++ run_experiments_s2s.sh
@@ -0,0 +1,9 @@
+##!/usr/bin/env bash
+
+datasets=("dailydialog" "personachat" "wizard_of_wikipedia" "moviedialog")
+
+for dataset in "${datasets[@]}"
+do
+  python -m parlai.scripts.train_model -t $dataset -m seq2seq -mf tmp/"$dataset"/seq2seq/model_s2s_2_512_gru_bi_final -nl 2 -hs 512 -rnn gru -esz 300 -bi True -att none --num-epochs 1 -veps 2.0 -bs 64 -opt adam -lr 0.001 --dropout 0.1 --embedding-type fasttext_cc  -vp 10 --inference greedy -vmt bleu-4
+  python -m parlai.scripts.self_chat -t $dataset:selfchat -m seq2seq -mf tmp/"$dataset"/seq2seq/model_s2s_2_512_gru_bi_final --inference nucleus
+done
