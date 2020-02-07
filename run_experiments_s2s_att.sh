2689d5e82968809009ef3c13fc490bd1eceffa78
Jan Deriu <deri@zhaw.ch>
2020-02-07 16:16:34 +0100

    s2s

--- run_experiments_s2s_att.sh
+++ run_experiments_s2s_att.sh
@@ -1,9 +1,9 @@
 ##!/usr/bin/env bash
 
-datasets=("dailydialog" "personachat" "wizard_of_wikipedia" "moviedialog:Task:4")
+datasets=("dailydialog" "personachat" "wizard_of_wikipedia" "cornell_movie")
 
 for dataset in "${datasets[@]}"
 do
-  python -m parlai.scripts.train_model -t $dataset -m seq2seq -mf tmp/"$dataset"/seq2seq_att/model_s2s_2_512_gru_bi_final -nl 2 -hs 512 -rnn gru -esz 300 -bi True -att general --num-epochs 50 -veps 2.0 -bs 64 -opt adam -lr 0.001 --dropout 0.1 --embedding-type fasttext_cc  -vp 10 --inference greedy -vmt bleu-4
-  python -m parlai.scripts.self_chat -t $dataset:selfchat -m seq2seq -mf tmp/"$dataset"/seq2seq_att/model_s2s_2_512_gru_bi_final --inference nucleus --outfile tmp/dailydialog/model_s2s_att_2_512_gru_bi_final.json
+  python -m parlai.scripts.train_model -t $dataset -m seq2seq -mf tmp/"$dataset"/seq2seq_att/model_s2s_2_512_gru_bi_final -nl 2 -hs 512 -rnn gru -esz 300 -bi True -att general --num-epochs 60 -veps 2.0 -bs 64 -opt adam -lr 0.001 --dropout 0.1 --embedding-type fasttext_cc  -vp 10 --inference greedy -vmt bleu-4
+  python -m parlai.scripts.self_chat -t $dataset:selfchat -m seq2seq -mf tmp/"$dataset"/seq2seq_att/model_s2s_2_512_gru_bi_final --inference nucleus --outfile tmp/"$dataset"/model_s2s_att_2_512_gru_bi_final.json
 done
