af0deea241e35e4266e585f568e83b7aba9751f8
Deriu Jan Milan <deri@gpulogin.cloudlab.zhaw.ch>
2020-02-14 14:31:08 +0100

    sctips

---
+++ run_cornell_seq2seq_att_experiments.sh
@@ -0,0 +1,10 @@
+##!/usr/bin/env bash
+export PYTHONPATH=.
+for nl in 1 2
+do
+  for hs in 256 512
+  do
+      python -m parlai.scripts.train_model -t cornell_movie -m seq2seq -mf tmp/cornell_movie/seq2seq_att/model_s2s_"$nl"_"$hs"_gru_bi -nl $nl -hs $hs -rnn gru -esz 300 -bi True -att general --num-epochs 50 -veps 1.0 -bs 32 --optimizer adam -lr 0.001 --dropout 0.1 --embedding-type fasttext_cc  -vp 50 --inference greedy -vmt bleu-4 -sval True
+      python parlai/scripts/self_chat.py  -t cornell_movie:selfchat -m seq2seq -mf tmp/cornell_movie/seq2seq_att/model_s2s_"$nl"_"$hs"_gru_bi --outfile tmp/cornell_movie/model_s2s_att_"$nl"_"$hs"_gru_bi.json
+  done
+done
